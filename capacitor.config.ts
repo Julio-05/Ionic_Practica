d0d36c6e8ca1653c1839fe1c14438b79eee77200
Julio-05 <masterjulio0510@gmail.com>
2023-09-27 07:18:49 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tarea_3_ionic_app_mobile',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
